842c21e115392925c34878521fc951c6c3b9b90f
mariana wismann <marianawis021@gmail.com>
2024-06-22 20:48:40 -0500

    estructura proyecto

---
+++ src/test/java/com/example/demo/ProyectoLp2Grupo04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLp2Grupo04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
